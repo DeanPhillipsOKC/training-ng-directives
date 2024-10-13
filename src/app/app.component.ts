306f0a432569c93d7903a96f38958f52c92e9062
Dean Phillips <dephillips1977@gmail.com>
2024-10-13 14:32:21 -0500

    85. Custom Structural Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ClassDirective } from './class.directive';
+import { TimesDirective } from './times.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, ClassDirective],
+  imports: [RouterOutlet, CommonModule, ClassDirective, TimesDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
